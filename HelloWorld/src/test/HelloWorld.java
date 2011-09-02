389fd1adc5527ba8f1f969a180df14bc8237a62c
akyao <akyao@221.170.30.129>
2011-09-02 09:16:34 +0900

    init

---
+++ HelloWorld/src/test/HelloWorld.java
@@ -0,0 +1,20 @@
+package test;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+	
+	private void branch1(){
+		System.out.println("branch1");
+	}
+
+	public void branch2Method(){
+		System.out.println("doraemon");
+	}
+}
